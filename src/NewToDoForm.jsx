5ff0e46ddbab04e5819e7ca67ef92381bc3c9ee8
Carl Damey <140026004+carldamey@users.noreply.github.com>
2023-11-12 07:27:08 +0000

    Update NewToDoForm.jsx

--- src/NewToDoForm.jsx
+++ src/NewToDoForm.jsx
@@ -2,19 +2,22 @@ import { useState } from "react";
 
 export default function NewToDoForm({ todos, setTodos }) {
   const [newTodo, setNewTodo] = useState("");
-  function handleAddTodo() {
+  function handleAddTodo(evt) {
+    evt.preventDefault();
     setTodos([...todos, newTodo]);
     setNewTodo("");
   }
   return (
-    <>
+    <form onSubmit={handleAddTodo}>
       <h2>New To-Do</h2>
       <input
         value={newTodo}
         onChange={(evt) => setNewTodo(evt.target.value)}
         placeholder="New To-Do"
+        required
+        pattern=".{4}"
       />
-      <button onClick={handleAddTodo}>ADD TO-DO</button>
-    </>
+      <button type="submit">ADD TO-DO</button>
+    </form>
   );
 }
